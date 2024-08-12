66d540308e8c3269bbc66a06151f8a1d8713fa43
MayankMeghani <120728455+MayankMeghani@users.noreply.github.com>
2024-08-12 08:16:59 +0530

    started

---
+++ MovieTicketBooking/HomePage.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="MovieTicketBooking.HomePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 299px">
+    <form id="form1" runat="server">
+        <div accesskey="rblMovie">
+            <asp:RadioButtonList ID="rblMovies" runat="server" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged">
+            </asp:RadioButtonList>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="rblMovies" ErrorMessage="Select movie">Please select movie</asp:RequiredFieldValidator>
+            <br />
+            <asp:Button ID="btnView" runat="server" OnClick="btnView_Click" Text="View" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ MovieTicketBooking/HomePage.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MovieTicketBooking
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = WebConfigurationManager.ConnectionStrings["MovieDbContext"].ConnectionString;
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "select * from Movies";
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    ListItem item = new ListItem();
+                    item.Text = reader["Title"].ToString();
+                    item.Value = reader["MovieId"].ToString();
+                    rblMovies.Items.Add(item);
+
+                }
+                con.Close();
+            }
+        }
+
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnView_Click(object sender, EventArgs e)
+        {
+                string selectedMovie = rblMovies.SelectedItem.Text;
+
+                Session["SelectedMovie"] = selectedMovie;
+
+                Response.Redirect("Schedule.aspx");
+        }
+
+    }
+}
